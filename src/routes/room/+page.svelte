067b9bbac21354b3314df921376cc8ada4ae9b63
Thaddeus Cleo <thaddeuscleo@gmail.com>
2022-11-22 12:46:28 +0700

    feat: create room table

--- src/routes/room/+page.svelte
+++ src/routes/room/+page.svelte
@@ -1,22 +1,16 @@
-<script>
-	import SoftwareDataTable from '../../components/SoftwareDataTable.svelte';
+<script lang="ts">
+	import RoomDataTable from './../../components/RoomDataTable.svelte';
 
-	const take = 10;
-	const currentPage = 1;
-
-	let softwareTableHeaders = ['Room Number', 'Master Type', 'Location'];
-	let softwares = [
-		{ id: '1', softwareName: 'test', currentLicense: 'true', group: 'autocad' },
-		{ id: '2', softwareName: 'test2', currentLicense: 'false', group: 'autocad' },
-		{ id: '3', softwareName: 'test3', currentLicense: 'true', group: 'autocad' }
-	];
+	export let data: any;
+	let { take, roomCount, numberOfPage } = data;
+	$: ({ rooms } = data);
+	$: ({ currentPage } = data);
 </script>
 
-<SoftwareDataTable
-	tableHeaders={softwareTableHeaders}
-	dataCollection={softwares}
-	numberOfPage={1}
-	allDataCount={softwares.length}
+<RoomDataTable
+	dataCollection={rooms}
+	allDataCount={roomCount}
+	{numberOfPage}
 	{currentPage}
 	{take}
 />
---
+++ src/routes/room/+page.ts
@@ -0,0 +1,42 @@
+import { PUBLIC_GRAPHQL_ENDPOINT } from '$env/static/public';
+import request, { gql } from 'graphql-request';
+
+export async function load({ url }: { url: URL }) {
+	const take = 10;
+
+	let param = url.searchParams.get('currentPage');
+	let currentPage = param === null || param <= '0' ? 1 : parseInt(param);
+	const skip = (currentPage - 1) * take;
+
+	const query = gql`
+		query get_rooms($skip: Int!, $take: Int!) {
+			rooms(skip: $skip, take: $take) {
+				id
+				roomNumber
+			}
+		}
+	`;
+
+	const { rooms } = await request(PUBLIC_GRAPHQL_ENDPOINT, query, {
+		skip,
+		take
+	});
+
+	const countQuery = gql`
+		query get_room_count {
+			roomCount
+		}
+	`;
+
+	const { roomCount } = await request(PUBLIC_GRAPHQL_ENDPOINT, countQuery);
+
+	const numberOfPage = Math.ceil(roomCount / take);
+
+	return {
+		rooms,
+		roomCount,
+		take,
+		currentPage,
+		numberOfPage
+	};
+}
